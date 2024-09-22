a762af834b2ad3cc59983c8be80fa7de47b0ef48
FernandoPinto2002 <ferfer192002@gmail.com>
2024-09-21 23:41:13 -0600

    mission

---
+++ Mission/mission.js
@@ -0,0 +1,13 @@
+const themeSelector = document.querySelector('#theme-selector');
+
+function changeTheme() {
+    if (themeSelector.value === 'dark') {
+        document.body.classList.add('dark');
+        document.getElementById('logo').src = 'images/byui-logo_white.png'; 
+    } else {
+        document.body.classList.remove('dark');
+        document.getElementById('logo').src = "images/byui-logo_blue.webp"; 
+    }
+}
+
+themeSelector.addEventListener('change', changeTheme);
