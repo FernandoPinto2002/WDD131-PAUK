186f271b0017c4f0186524868422df97773a1122
FernandoPinto2002 <ferfer192002@gmail.com>
2024-09-28 23:53:19 -0600

    changes

---
+++ coolpics/coolpics.js
@@ -0,0 +1,75 @@
+const menuButton = document.querySelector(".menu-button");
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("hide");
+
+    
+    if (!menu.classList.contains("hide")) {
+        const navItems = document.querySelectorAll("nav li");
+        navItems.forEach(item => item.style.display = "block");
+    } else {
+       
+        const navItems = document.querySelectorAll("nav li");
+        navItems.forEach(item => item.style.display = "none");
+    }
+}
+
+menuButton.addEventListener("click", toggleMenu);
+
+function handleResize() {
+    const menu = document.querySelector(".menu");
+    const navItems = document.querySelectorAll("nav li");
+    
+    if (window.innerWidth > 1000) {
+        menu.classList.remove("hide");
+        navItems.forEach(item => item.style.display = "block");
+    } else if (window.innerWidth > 500 && window.innerWidth <= 750) {
+        menu.classList.add("hide");
+        navItems.forEach(item => item.style.display = "none");
+    } else {
+        menu.classList.add("hide");
+        navItems.forEach(item => {
+            item.style.display = "block"; 
+            item.style.display = "flex";
+        });
+    }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
+
+
+function viewerTemplate(pic, alt) {
+    return `
+        <div class="viewer">
+            <button class="close-viewer">X</button>
+            <img src="${pic}" alt="${alt}">
+        </div>`;
+}
+
+function viewHandler(event) {
+    const clickedElement = event.target;
+
+    if (clickedElement.tagName === 'IMG') {
+        const imageSrc = clickedElement.src.split("-")[0] + "-full.jpeg"; 
+        const htmlToInsert = viewerTemplate(imageSrc, clickedElement.alt);
+        document.body.insertAdjacentHTML("afterbegin", htmlToInsert); 
+        const closeButton = document.querySelector(".close-viewer");
+        closeButton.addEventListener("click", closeViewer);
+    }
+}
+
+
+function closeViewer() {
+    const viewer = document.querySelector(".viewer");
+    if (viewer) {
+        viewer.remove(); 
+    }
+}
+
+
+
+
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", viewHandler);
---
+++ sandbox.js
@@ -0,0 +1,6 @@
+function addNumbers() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let sum = num1 + num2;
+    console.log("The sum is: " + sum);
+}
